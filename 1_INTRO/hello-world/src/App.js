74fa79af48eb500d3827998931b8d16351043a30
Ewerson <pulquerio.x@gmail.com>
2022-04-29 21:06:13 -0300

    Primeiro upload do Curso de React, introdução

---
+++ 1_INTRO/hello-world/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Formulário principal</h1>
+      <h2>Emmet Funcionou</h2>
+      Antes de iniciar sempre entrar na pasta hello-world através do comando <b class="teste">cd.</b> e depois disso digitar: <i class="teste">npm start</i>.
+    </div>
+  );
+}
+
+export default App;
