cc11a508a0af6ac18d78f4fba222d19a18db82bb
Ewerson <pulquerio.x@gmail.com>
2022-04-29 21:25:36 -0300

    Arquivo de informações melhorado!

--- 1_INTRO/hello-world/src/App.js
+++ 1_INTRO/hello-world/src/App.js
@@ -7,6 +7,9 @@ function App() {
       <h1>Formulário principal</h1>
       <h2>Emmet Funcionou</h2>
       Antes de iniciar sempre entrar na pasta hello-world através do comando <b class="teste">cd.</b> e depois disso digitar: <i class="teste">npm start</i>.
+      <div>
+        <a href="comentarios.html">Clique aqui</a> para acessar todas as informações.
+      </div>
     </div>
   );
 }
