c9e849901195b5cbb7c75a97af124270ac3b2394
Ewerson Pulquerio <pulquerio.x@gmail.com>
2022-05-06 19:58:47 -0300

    Finalizei a aula 25 de Funções de Renderização

--- 2_FUNDAMENTOS/fundamentos/src/components/Events.js
+++ 2_FUNDAMENTOS/fundamentos/src/components/Events.js
@@ -5,6 +5,15 @@ const Events = () => {
         console.log("Ativou o evento!");
     };
 
+    const renderSomething = (x) => {
+        if (x) {
+            return <p>Renderizando isso</p>
+        } else {
+            return <p>Também posso renderizar isso</p>
+        }
+
+
+    }
     return (
         <div>
             <div>
@@ -15,11 +24,13 @@ const Events = () => {
             </div>
             <div>
                 <button onClick={() => {
-                    if(true){
-                        console.log("Isso não deveria existir!");
+                    if (true) {
+                        console.log("Isso não deveria existir!"); //NÃO FAZER ISSO!
                     }
                 }}>Clique aqui, por favor</button>
             </div>
+            {renderSomething(true)}
+            {renderSomething(false)}
         </div>
     );
 };
