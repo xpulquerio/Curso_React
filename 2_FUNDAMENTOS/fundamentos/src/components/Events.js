2f9f642b0ff64e04b65005da868667836aee4e13
Ewerson Pulquerio <pulquerio.x@gmail.com>
2022-05-06 19:23:36 -0300

    Finalizei a aula 24 de Funções em Eventos

--- 2_FUNDAMENTOS/fundamentos/src/components/Events.js
+++ 2_FUNDAMENTOS/fundamentos/src/components/Events.js
@@ -1,7 +1,7 @@
 const Events = () => {
 
     const handleMyEvent = (e) => {
-        console.log(e);
+        //console.log(e);
         console.log("Ativou o evento!");
     };
 
@@ -9,6 +9,16 @@ const Events = () => {
         <div>
             <div>
                 <button onClick={handleMyEvent}>Clique aqui!</button> {/*Se colocar o parenteses em handleMyEvento(), ele ativa o evento assim que ler o elemento, por isso deixamos sem parenteses*/}
+            </div>
+            <div>
+                <button onClick={() => console.log("Clicou!")}>Clique aqui também!</button>
+            </div>
+            <div>
+                <button onClick={() => {
+                    if(true){
+                        console.log("Isso não deveria existir!");
+                    }
+                }}>Clique aqui, por favor</button>
             </div>
         </div>
     );
