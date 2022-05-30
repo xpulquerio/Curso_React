b4feec9635f5cc888e333d6701eabc46733a7038
Ewerson Pulquerio <pulquerio.x@gmail.com>
2022-05-30 11:29:56 -0300

    Post (envio) de informações para o banco de dados!

--- 7_REQ_HTTP/httpreact/src/App.js
+++ 7_REQ_HTTP/httpreact/src/App.js
@@ -8,6 +8,9 @@ function App() {
 
   const [products, setProducts] = useState([]);
 
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
+
   //1 - resgatando dados
   useEffect(() => {
     async function fetchData() {
@@ -22,15 +25,70 @@ function App() {
 
   }, []);
 
+  //2 - add de produtos
+  const handleSubmit = async (e) => {
+
+    e.preventDefault();
+
+    const product = {
+      name,
+      price,
+    };
+
+    console.log(product); //comentário
+
+    const res = await fetch(url, {
+      method: "POST",
+      headers: {
+        "content-Type": "application/json"
+      },
+      body: JSON.stringify(product),
+    });
+    
+    //3 - Carregamento dinâmico
+    const addedProduct = await res.json();
+
+    setProducts((prevProducts) => [...prevProducts, addedProduct]);
+
+    setName("");
+    setPrice("");
+  };
+
 
   return (
     <div className="App">
       <h1>Lista de Produtos</h1>
       <ul>
         {products.map((product) => (
-          <li key={product.id}>{product.name} - R$: {product.price}</li>
+          <li key={product.id}>{product.name} - {product.price ? ("R$: "+product.price) : ("Sem preço")}</li>
         ))}
       </ul>
+
+      <div className="add-product">
+        <form onSubmit={handleSubmit}>
+          <label>
+            Nome:
+            <input
+              type="text"
+              value={name}
+              name="name"
+              onChange={(e) => setName(e.target.value)}
+            />
+          </label>
+          <label>
+            Preço:
+            <input
+              type="text"
+              value={price}
+              name="price"
+              onChange={(e) => setPrice(e.target.value)}
+            />
+          </label>
+          <input type="submit" value="Criar" />
+        </form>
+
+      </div>
+
     </div>
   );
 }
