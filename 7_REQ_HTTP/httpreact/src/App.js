b254b24081c11b113f2716bb3248b6bc4a44795f
Ewerson Pulquerio <pulquerio.x@gmail.com>
2022-06-01 21:59:22 -0300

    Avançado até aula 89

--- 7_REQ_HTTP/httpreact/src/App.js
+++ 7_REQ_HTTP/httpreact/src/App.js
@@ -1,29 +1,35 @@
 import './App.css';
 
 import { useState, useEffect } from "react";
+// 4 - custom hook
+import { useFetch } from './hooks/useFetch';
 
 const url = "http://localhost:3000/products"
 
 function App() {
 
-  const [products, setProducts] = useState([]);
+
+
+  //4 - custom hook
+
+  const { data: items, httpConfig, loading } = useFetch(url) //renomeando como items.
 
   const [name, setName] = useState("");
   const [price, setPrice] = useState("");
 
   //1 - resgatando dados
-  useEffect(() => {
-    async function fetchData() {
-      const res = await fetch(url)
-
-      const data = await res.json()
-
-      setProducts(data)
-    }
-
-    fetchData();
-
-  }, []);
+  //    useEffect(() => {
+  //    async function fetchData() {
+  //     const res = await fetch(url)
+  //
+  //      const data = await res.json()
+  //
+  //       setProducts(data)
+  //}
+  //
+  //fetchData();
+  //
+  //}, []);
 
   //2 - add de produtos
   const handleSubmit = async (e) => {
@@ -35,20 +41,22 @@ function App() {
       price,
     };
 
-    console.log(product); //comentário
+    //    const res = await fetch(url, {
+    //      method: "POST",
+    //      headers: {
+    //        "content-Type": "application/json"
+    //      },
+    //      body: JSON.stringify(product),
+    //    });
+    //
+    //    //3 - Carregamento dinâmico
+    //    const addedProduct = await res.json();
+    //
+    //    setProducts((prevProducts) => [...prevProducts, addedProduct]);
 
-    const res = await fetch(url, {
-      method: "POST",
-      headers: {
-        "content-Type": "application/json"
-      },
-      body: JSON.stringify(product),
-    });
-    
-    //3 - Carregamento dinâmico
-    const addedProduct = await res.json();
+    //5 - refatorando post
 
-    setProducts((prevProducts) => [...prevProducts, addedProduct]);
+    httpConfig(product, "POST");
 
     setName("");
     setPrice("");
@@ -58,11 +66,15 @@ function App() {
   return (
     <div className="App">
       <h1>Lista de Produtos</h1>
-      <ul>
-        {products.map((product) => (
-          <li key={product.id}>{product.name} - {product.price ? ("R$: "+product.price) : ("Sem preço")}</li>
-        ))}
-      </ul>
+      {/* 6 - loading */}
+      {loading && <p>Carregando dados...</p>}
+      {!loading &&
+        <ul>
+          {items && items.map((product) => ( //Se não for null, imprime os itens!
+            <li key={product.id}>{product.name} - {product.price ? ("R$: " + product.price) : ("Sem preço")}</li>
+          ))}
+        </ul>
+      }
 
       <div className="add-product">
         <form onSubmit={handleSubmit}>
